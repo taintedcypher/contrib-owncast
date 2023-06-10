084e3427d8852073e52c2d76610279575a875a23
Gabe Kangas <gabek@real-ity.com>
2023-06-09 17:29:02 -0700

    feat: first pass migrating all the handlers to the new architecture

---
+++ webserver/handlers/index.go
@@ -0,0 +1,95 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"path/filepath"
+	"strings"
+
+	"github.com/owncast/owncast/core/data"
+	"github.com/owncast/owncast/static"
+	"github.com/owncast/owncast/utils"
+	"github.com/owncast/owncast/webserver/middleware"
+)
+
+// IndexHandler handles the default index route.
+func (h *Handlers) IndexHandler(w http.ResponseWriter, r *http.Request) {
+	middleware.EnableCors(w)
+
+	isIndexRequest := r.URL.Path == "/" || filepath.Base(r.URL.Path) == "index.html" || filepath.Base(r.URL.Path) == ""
+
+	if utils.IsUserAgentAPlayer(r.UserAgent()) && isIndexRequest {
+		http.Redirect(w, r, "/hls/stream.m3u8", http.StatusTemporaryRedirect)
+		return
+	}
+
+	// Set a cache control max-age header
+	middleware.SetCachingHeaders(w, r)
+
+	nonceRandom, _ := utils.GenerateRandomString(5)
+
+	// Set our global HTTP headers
+	middleware.SetHeaders(w, fmt.Sprintf("nonce-%s", nonceRandom))
+
+	if isIndexRequest {
+		renderIndexHtml(w, nonceRandom)
+		return
+	}
+
+	serveWeb(w, r)
+}
+
+func renderIndexHtml(w http.ResponseWriter, nonce string) {
+	type serverSideContent struct {
+		Name             string
+		Summary          string
+		RequestedURL     string
+		TagsString       string
+		ThumbnailURL     string
+		Thumbnail        string
+		Image            string
+		StatusJSON       string
+		ServerConfigJSON string
+		EmbedVideo       string
+		Nonce            string
+	}
+
+	status := getStatusResponse()
+	sb, err := json.Marshal(status)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	config := getConfigResponse()
+	cb, err := json.Marshal(config)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	content := serverSideContent{
+		Name:             data.GetServerName(),
+		Summary:          data.GetServerSummary(),
+		RequestedURL:     fmt.Sprintf("%s%s", data.GetServerURL(), "/"),
+		TagsString:       strings.Join(data.GetServerMetadataTags(), ","),
+		ThumbnailURL:     "thumbnail.jpg",
+		Thumbnail:        "thumbnail.jpg",
+		Image:            "logo/external",
+		StatusJSON:       string(sb),
+		ServerConfigJSON: string(cb),
+		EmbedVideo:       "embed/video",
+		Nonce:            nonce,
+	}
+
+	index, err := static.GetWebIndexTemplate()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := index.Execute(w, content); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
